fd13c7ada1428b120b8b3433790ed72014014911
MadV313 <snowvonia@gmail.com>
2025-09-06 12:02:48 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -115,6 +115,7 @@ async def main():
         await BOT.load_extension("cogs.admin_misc")
         await BOT.load_extension("cogs.help")
         await BOT.load_extension("cogs.show_tracked")
+        await BOT.load_extension("cogs.links_normalize")
 
         token = os.environ.get("DISCORD_TOKEN")
         if not token:
